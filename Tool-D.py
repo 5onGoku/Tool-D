2446d14de47202747964bf62921fe4f18519c1ee
Ayush kunwar <69688840+5onGoku@users.noreply.github.com>
2021-10-02 02:09:17 +0530

    Update Tool-D.py

--- Tool-D.py
+++ Tool-D.py
@@ -9,72 +9,83 @@ banner = '''
 
 print(banner)
 
-print("[0] update Tool-D to newest version")
-print("[1] TBomb")
-print("[2] zphisher")
-print("[3] Hidden-Eye")
-print("[4] wifite")
-print("[5] bopscrk")
-print("[6] GHunt")
-print("[7] pspy")
-print("[8] snitch")
-print("[9] seeker")
-print("[10] sublist3r")
-print("[11] knock")
-print("[12] blackeye")
-print("[13] weeman")
-print("[14] userrecon")
-print("[15] sherlock")
-print("[16] Osintgram")
+def downloadables():
+        print("[0] update Tool-D to newest version")
+        print("[1] TBomb")
+        print("[2] zphisher")
+        print("[3] Hidden-Eye")
+        print("[4] wifite")
+        print("[5] bopscrk")
+        print("[6] GHunt")
+        print("[7] pspy")
+        print("[8] snitch")
+        print("[9] seeker")
+        print("[10] sublist3r") 
+        print("[11] knock")
+        print("[12] blackeye")
+        print("[13] weeman")
+        print("[14] userrecon")
+        print("[15] sherlock")
+        print("[16] Osintgram")
 
-print(" ")
-c=int(input("Enter tool no. you want to install: "))
+        print(" ")
 
-if c==1:
-    os.system('git clone https://github.com/TheSpeedX/TBomb.git')
-elif c==2:
-    os.system('git clone https://github.com/htr-tech/zphisher.git')
-elif c==3:
-    os.system('git clone https://github.com/darkmidus/HiddenEye.git')
-elif c==4:
-    os.system('git clone https://github.com/derv82/wifite.git')
-elif c==5:
-    os.system('git clone https://github.com/r3nt0n/bopscrk.git')
-elif c==6:
-    os.system('git clone https://github.com/mxrch/GHunt.git')
-elif c==7:
-    os.system('git clone https://github.com/DominicBreuker/pspy.git')
-elif c==8:
-    os.system('git clone https://github.com/Smaash/snitch.git')
-elif c==9:
-    os.system('git clone https://github.com/thewhiteh4t/seeker.git')
-elif c==10:
-    os.system('git clone https://github.com/aboul3la/Sublist3r.git')
-elif c==11:
-    os.system('git clone https://github.com/guelfoweb/knock.git')
-elif c==0:
-    os.system('git clone https://github.com/dellucifer/Tool-D.git')
-elif c==12:
-    os.system('git clone https://github.com/An0nUD4Y/blackeye.git')
-elif c==13:
-    os.system('git clone https://github.com/evait-security/weeman.git')
-elif c==14:
-    os.system('git clone https://github.com/issamelferkh/userrecon.git')
-elif c==15:
-    os.system('git clone https://github.com/sherlock-project/sherlock.git')
-elif c==16:
-    os.system('git clone https://github.com/Datalux/Osintgram.git')
-elif c==17:
-    os.system('https://github.com/suljot/shellphish.git')
-elif c==18:
-    os.system('https://github.com/jaykali/maskphish.git')
-elif c==19:
-    os.system('https://github.com/techchipnet/CamPhish.git')
-elif c==20:
-    os.system('https://github.com/htr-tech/nexphisher.git')
-else:
-    print("Invalid Choice")
 
-print(" ")
+while True:
+    downloadables()
+    c = int(input("Enter tool no. you want to install: "))
+
+    if c==1:
+        os.system('git clone https://github.com/TheSpeedX/TBomb.git')
+    elif c==2:
+        os.system('git clone https://github.com/htr-tech/zphisher.git')
+    elif c==3:
+        os.system('git clone https://github.com/darkmidus/HiddenEye.git')
+    elif c==4:
+        os.system('git clone https://github.com/derv82/wifite.git')
+    elif c==5:
+        os.system('git clone https://github.com/r3nt0n/bopscrk.git')
+    elif c==6:
+        os.system('git clone https://github.com/mxrch/GHunt.git')
+    elif c==7:
+        os.system('git clone https://github.com/DominicBreuker/pspy.git')
+    elif c==8:
+        os.system('git clone https://github.com/Smaash/snitch.git')
+    elif c==9:
+        os.system('git clone https://github.com/thewhiteh4t/seeker.git')
+    elif c==10:
+        os.system('git clone https://github.com/aboul3la/Sublist3r.git')
+    elif c==11:
+        os.system('git clone https://github.com/guelfoweb/knock.git')
+    elif c==0:
+        os.system('git clone https://github.com/dellucifer/Tool-D.git')
+    elif c==12:
+        os.system('git clone https://github.com/An0nUD4Y/blackeye.git')
+    elif c==13:
+        os.system('git clone https://github.com/evait-security/weeman.git')
+    elif c==14:
+        os.system('git clone https://github.com/issamelferkh/userrecon.git')
+    elif c==15:
+        os.system('git clone https://github.com/sherlock-project/sherlock.git')
+    elif c==16:
+        os.system('git clone https://github.com/Datalux/Osintgram.git')
+    elif c==17:
+        os.system('https://github.com/suljot/shellphish.git')
+    elif c==18:
+        os.system('https://github.com/jaykali/maskphish.git')
+    elif c==19:
+        os.system('https://github.com/techchipnet/CamPhish.git')
+    elif c==20:
+        os.system('https://github.com/htr-tech/nexphisher.git')
+    else:
+        print("Invalid Choice")
+        continue
+    print("Do you want to install another tool.")
+    inp =  input("Choose Y/N: ")
+    if inp == "Y" or inp == "Yes" or inp == "y":
+        continue
+    elif inp == "N" or inp == "No" or inp == "n":
+        break
+        print(" ")
 print("Thanks for using Tool-D")
 print("AUTHOR: DELLUCIFER")
